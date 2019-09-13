0d1170b12367fd4befe6c18a405dc335588868fb
abrildelburgo <pildelburgo@gmail.com>
2019-09-13 00:51:37 -0300

    main de memoria

--- Memoria/muse.c
+++ Memoria/muse.c
@@ -3,7 +3,11 @@
 #include "utils.h"
 
 int main(){
-
+	// Levanta archivo de configuracion
+	t_config* configuracion = leer_config();
+	t_log *log = crear_log();
+	char* ip = config_get_string_value(configuracion, "IP");
+	int puerto = config_get_int_value(configuracion, "LISTEN_PORT");
 }
 
 
--- Memoria/utils.c
+++ Memoria/utils.c
@@ -1,2 +1,10 @@
 #include "utils.h"
 
+t_config* leer_config() {
+	return config_create("suse_config");
+}
+
+
+t_log * crear_log() {
+	return log_create("suse.log", "suse", 1, LOG_LEVEL_DEBUG);
+}
