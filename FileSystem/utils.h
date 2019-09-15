7bb8e64569faab35d2a9115219ffe2fc876d53a9
Emilio Nogueiras <emilio.nogueiras@gmail.com>
2019-09-15 20:20:16 -0300

    Se agregan struct de header y de nodos en utils.h

--- FileSystem/utils.h
+++ FileSystem/utils.h
@@ -4,12 +4,13 @@
 
 #include<sys/socket.h>
 #include<stdio.h>
-#include<string.h>
-#include<netdb.h>
-#include<unistd.h>
-#include<commons/config.h>
-#include<commons/string.h>
-#include<commons/log.h>
+#include <string.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/log.h>
+#include <commons/bitarray.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <arpa/inet.h>
@@ -21,6 +22,45 @@
 #include <dirent.h>
 #include <pthread.h>
 
+#define GFILEBYTABLE 1024
+#define GFILEBYBLOCK 1
+#define GFILENAMELENGTH 71
+#define GHEADERBLOCKS 1
+#define BLKINDIRECT 1000
+#define BLOCK_SIZE 4096
+
+
+typedef uint32_t ptrGBloque;
+
+typedef enum __attribute__((packed)){
+	BORRADO = '\0',
+	OCUPADO = '\1',
+	DIRECTORIO = '\2'
+}estado_archivo;
+
+typedef struct header_t { // un bloque (4096 bytes)
+	unsigned char identificador[3]; // valor "SAC"
+	uint32_t version; // valor 1
+	ptrGBloque bloque_inicio_bitmap; // valor 1
+	uint32_t tamanio_bitmap; // valor n (bloques)
+	unsigned char relleno[4081]; // padding
+} GHeader;
+
+typedef struct file_t {
+	estado_archivo estado; // 0: borrado, 1: archivo, 2: directorio
+	unsigned char nombre_archivo[GFILENAMELENGTH]; // valor nombre del archivo
+	ptrGBloque bloque_padre; // nro de bloque del directorio padre, 0 si esta en el raiz
+	uint32_t tamanio_archivo; // maximo tamanio de archivo 4GB
+	uint64_t fecha_creacion;
+	uint64_t fecha_modificacion;
+	ptrGBloque bloques_indirectos[BLKINDIRECT];
+} GFile;
+
+typedef struct {
+	int ip;
+	int puerto;
+} config;
+
 int iniciar_conexion(int, int);
 void levantarConfigFile();
 t_config* leer_config(void);
@@ -29,9 +69,6 @@ t_log * crear_log();
 t_log* logger;
 pthread_t hiloLevantarConexion;
 
-typedef struct {
-	int ip;
-	int puerto;
-} config;
+
 
 #endif /* UTILS_H_ */
