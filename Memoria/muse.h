769152d4d1748b29270d4b570646e0c06fce7f1a
Fiam <coedo.fiamma@gmail.com>
2019-09-19 15:45:04 -0300

    Modifico estructuras. HAgo algunos includes necesarios

--- Memoria/muse.h
+++ Memoria/muse.h
@@ -1,15 +1,23 @@
 #ifndef MUSE_H_
 #define MUSE_H_
+#include <stdint.h>
+#include<commons/collections/list.h>
 
-void* memoriaPrincipal;
+char* memoriaPrincipal;
 
 typedef struct {
-
+	bool uso;
+	uint32_t size;
+	t_list* paginas;
+	//char* data; Ver - segun el mail esta en segmento, yo creo que esta en cada pagina.
 } segmento;
 
 typedef struct {
-
+	int numeroPagina;
+	bool modificado;
+	bool uso;
+	bool presencia;
+	char* data;
 } pagina;
-
 
 #endif /* MUSE_H_ */
