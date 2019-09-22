4bf4ba75fbecfc8ce491aae490101cb9e87ec3c4
nachoCasciaro <icasciaro@est.frba.utn.edu.ar>
2019-09-22 17:51:46 -0300

    serializo map y corrijo errores

--- Memoria/libmuse.c
+++ Memoria/libmuse.c
@@ -76,7 +76,8 @@ int muse_get(void* dst, uint32_t src, size_t n) { //Case 5
 	//Serializo peticion (5), void* dst ,uint32_t src y size_t n
 
 	//4 size de tamanio, 1 size de peticion, 1 size de dst, 1 size de src y 1 size de n
-	char *buffer = malloc(5 * sizeof(int) + sizeof(dst) + sizeof(uint32_t) + sizeof(size_t));
+	char *buffer = malloc(
+			5 * sizeof(int) + sizeof(dst) + sizeof(uint32_t) + sizeof(size_t));
 
 	int peticion = 5;
 	int tamanioPeticion = sizeof(int);
@@ -88,12 +89,12 @@ int muse_get(void* dst, uint32_t src, size_t n) { //Case 5
 	memcpy(buffer + 3 * sizeof(int), &dst, sizeof(dst));
 
 	int tamanioSrc = sizeof(uint32_t);
-	memcpy(buffer + 3 * sizeof(int), &tamanioSrc, sizeof(int));
-	memcpy(buffer + 4 * sizeof(int), &src, sizeof(uint32_t));
+	memcpy(buffer + 3 * sizeof(int) + sizeof(dst), &tamanioSrc, sizeof(int));
+	memcpy(buffer + 4 * sizeof(int) + sizeof(dst), &src, sizeof(uint32_t));
 
 	int tamanioN = sizeof(size_t);
-	memcpy(buffer + 4 * sizeof(int), &tamanioN, sizeof(int));
-	memcpy(buffer + 5 * sizeof(int), &n, sizeof(size_t));
+	memcpy(buffer + 4 * sizeof(int) + sizeof(dst) , &tamanioN, sizeof(int));
+	memcpy(buffer + 5 * sizeof(int) + sizeof(dst), &n, sizeof(size_t));
 
 	//Falta conexion y se hace envio a MUSE
 	send(clienteMUSE, buffer, 5 * sizeof(int) + sizeof(dst) + sizeof(uint32_t) + sizeof(size_t), 0);
@@ -101,7 +102,7 @@ int muse_get(void* dst, uint32_t src, size_t n) { //Case 5
 	return 0;
 }
 
-int muse_cpy(uint32_t dst, void* src, int n){//Case 6
+int muse_cpy(uint32_t dst, void* src, int n) { //Case 6
 	//Serializo peticion (6) y parametros (uint32_t dst, void* src, int n)
 
 	char *buffer = malloc(6 * sizeof(int) + sizeof(uint32_t) + sizeof(src));
@@ -120,18 +121,41 @@ int muse_cpy(uint32_t dst, void* src, int n){//Case 6
 	memcpy(buffer + 4 * sizeof(int) + sizeof(uint32_t), &src, sizeof(src));
 
 	int tamanioN = sizeof(n);
-	memcpy(buffer + 4 * sizeof(int) + sizeof(uint32_t), &tamanioN, sizeof(int));
-	memcpy(buffer + 5 * sizeof(int) + sizeof(uint32_t), &n, sizeof(int));
-
+	memcpy(buffer + 4 * sizeof(int) + sizeof(uint32_t) + sizeof(src), &tamanioN, sizeof(int));
+	memcpy(buffer + 5 * sizeof(int) + sizeof(uint32_t) + sizeof(src), &n, sizeof(int));
 
 	//Falta conexion y se hace envio a MUSE
-	//send(clienteMUSE, buffer, , 0);
+	send(clienteMUSE, buffer, 6 * sizeof(int) + sizeof(uint32_t) + sizeof(src) , 0);
 
-    return 0;
+	return 0;
 
 }
 
 uint32_t muse_map(char *path, size_t length, int flags) { //Case 7
+	//Serializo peticion (7) y parametros (char* path, size_t length, int flags)
+
+	char *buffer = malloc(6 * sizeof(int) + strlen(path) + sizeof(size_t));
+
+	int peticion = 7;
+	int tamanioPeticion = sizeof(int);
+	memcpy(buffer, &tamanioPeticion, sizeof(int));
+	memcpy(buffer + sizeof(int), &peticion, sizeof(int));
+
+	int tamanioPath = strlen(path);
+	memcpy(buffer + 2 * sizeof(int), &tamanioPath, sizeof(int));
+	memcpy(buffer + 3 * sizeof(int), &path, strlen(path));
+
+	int tamanioLength = sizeof(size_t);
+	memcpy(buffer + 3 * sizeof(int) + strlen(path), &tamanioLength,sizeof(int));
+	memcpy(buffer + 4 * sizeof(int) + strlen(path), &length, sizeof(size_t));
+
+	int tamanioFlags = sizeof(int);
+	memcpy(buffer + 4 * sizeof(int) + strlen(path) + sizeof(size_t) , &tamanioFlags, sizeof(int));
+	memcpy(buffer + 5 * sizeof(int) + strlen(path) + sizeof(size_t), &flags, sizeof(int));
+
+	//Falta conexion y se hace envio a MUSE
+	send(clienteMUSE, buffer,6 * sizeof(int) + strlen(path) + sizeof(size_t) , 0);
+
 	return 0;
 }
 
@@ -150,11 +174,11 @@ int muse_sync(uint32_t addr, size_t len) { //Case 8
 	memcpy(buffer + 3 * sizeof(int), &addr, sizeof(uint32_t));
 
 	int tamanioLen = sizeof(len);
-	memcpy(buffer + 3 * sizeof(int) + sizeof(uint32_t), &tamanioLen, sizeof(int));
+	memcpy(buffer + 3 * sizeof(int) + sizeof(uint32_t), &tamanioLen,sizeof(int));
 	memcpy(buffer + 4 * sizeof(int) + sizeof(uint32_t), &len, sizeof(size_t));
 
 	//Falta conexion y se hace envio a MUSE
-	//send(clienteMUSE, buffer, , 0);
+	send(clienteMUSE, buffer,4 * sizeof(int) + sizeof(uint32_t) + sizeof(size_t) , 0);
 
 	return 0;
 }
@@ -174,7 +198,7 @@ int muse_unmap(uint32_t dir) { //Case 9
 	memcpy(buffer + 3 * sizeof(int), &dir, sizeof(uint32_t));
 
 	//Falta conexion y se hace envio a MUSE
-	//send(clienteMUSE, buffer, , 0);
+	send(clienteMUSE, buffer,3 * sizeof(int) + sizeof(uint32_t) , 0);
 
 	return 0;
 }
